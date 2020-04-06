b5271c4cffa16a0f44d25d4a7d665f3acb7e9eeb
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-04-06 10:02:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 app.set("view engine", "ejs"); 
 
 // TODO: Use correct path for DB
-mongoose.connect('mongodb+srv://adam:adam@hackatbrown-jtc8a.gcp.mongodb.net/test?retryWrites=true&w=majority', 
+mongoose.connect(MONGO_URI, 
 {useNewUrlParser: true});
 
 const db = mongoose.connection
